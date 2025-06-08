bc2398ec4af5c20353957dfad74188526d4b95cf
erickmufath <90117993+erickmufath@users.noreply.github.com>
2025-06-08 13:28:14 +0700

    Create install-dependencies.sh

---
+++ install-dependencies.sh
@@ -0,0 +1,3 @@
+pacman -S ttf-jetbrains-mono-nerd kitty
+read -p "CTRL+C dan install dulu Chaotic-AUR jika belum..." go
+sudo pacman -S chaotic-aur/hyprland wofi brightnessctl pamixer dunst swww waybar wlsunset udiskie blueman wlr-randr
