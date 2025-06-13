dd4e94c33785b1566a9933c589112e0131481a54
erickmufath <90117993+erickmufath@users.noreply.github.com>
2025-06-13 16:08:55 +0700

    Update install-dependencies.sh

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,3 +1,3 @@
 pacman -S ttf-jetbrains-mono-nerd kitty
 read -p "CTRL+C dan install dulu Chaotic-AUR jika belum..." go
-sudo pacman -S chaotic-aur/hyprland wofi brightnessctl pamixer dunst swww waybar wlsunset udiskie blueman wlr-randr
+sudo pacman -S chaotic-aur/hyprland hyprlock hypridle wofi brightnessctl pamixer dunst libnotify jq fastfetch kitty mpv easyeffects ladspa pipewire gst-plugin-pipewire pipewire-alsa pipewire-jack wireplumber pipewire-pulse libvdpau-va-gl vulkan-intel intel-ucode swww waybar wlsunset udiskie blueman wlr-randr intel-media-driver intel-media-sdk xdg-desktop-portal-hyprland xdg-desktop-portal-gtk 
