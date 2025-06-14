e90415aca80c77e106e7e4c9a4a1953acae72a7f
erickmufath <90117993+erickmufath@users.noreply.github.com>
2025-06-14 21:03:21 +0700

    Update install-dependencies.sh

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,31 +1,4 @@
 echo    "--------------------------------------------------------"
-read -p    "->] Install Intel Package [Y/n] : " intelpkg
-echo    "--------------------------------------------------------"
-case $intelpkg in
-Y|y|Yes|YES|yes)
-sudo pacman -S vulkan-intel intel-ucode intel-media-driver intel-media-sdk intel-gpu-tools libva-intel-driver intel-hybrid-codec-driver-git libvpl
-# Teks yang ingin ditambahkan
-block_to_add=$(cat <<EOF
-options i915 enable_guc=3
-EOF
-)
-# File konfigurasi yang ditarget
-config_file="/etc/modprobe.d/i915.conf"
-# Cek apakah blok sudah ada
-if ! grep -Fxq "options i915 enable_guc=3" "$config_file"; then
-    echo "Menambahkan GUC intel i915 ke $config_file agar aktif..."
-    echo "$block_to_add" | sudo tee -a "$config_file" > /dev/null
-    sudo mkinitcpio -p linux
-else
-    echo "Blok GUC Intel i915 sudah ada di $config_file. Tidak ditambahkan lagi."
-fi
-;;
-*)
-;;
-esac
-
-sudo pacman -S ttf-jetbrains-mono-nerd kitty hyprlock hypridle wofi brightnessctl pamixer dunst libnotify jq fastfetch kitty mpv easyeffects ladspa pipewire lsp-plugins pulsemixer gst-plugin-pipewire pipewire-alsa pipewire-jack wireplumber pipewire-pulse swww waybar wlsunset udiskie blueman wlr-randr xdg-desktop-portal-hyprland xdg-desktop-portal-gtk 
-echo    "--------------------------------------------------------"
 read -p    "->] Install Chaotic-AUR [Y/n] : " chaur
 echo    "--------------------------------------------------------"
 case $chaur in
@@ -51,6 +24,32 @@ fi
 *)
 ;;
 esac
-
 sudo pacman -Syu
+echo    "--------------------------------------------------------"
+read -p    "->] Install Intel Package [Y/n] : " intelpkg
+echo    "--------------------------------------------------------"
+case $intelpkg in
+Y|y|Yes|YES|yes)
+sudo pacman -S vulkan-intel intel-ucode intel-media-driver intel-media-sdk intel-gpu-tools libva-intel-driver intel-hybrid-codec-driver-git libvpl
+# Teks yang ingin ditambahkan
+block_to_add=$(cat <<EOF
+options i915 enable_guc=3
+EOF
+)
+# File konfigurasi yang ditarget
+config_file="/etc/modprobe.d/i915.conf"
+# Cek apakah blok sudah ada
+if ! grep -Fxq "options i915 enable_guc=3" "$config_file"; then
+    echo "Menambahkan GUC intel i915 ke $config_file agar aktif..."
+    echo "$block_to_add" | sudo tee -a "$config_file" > /dev/null
+    sudo mkinitcpio -p linux
+else
+    echo "Blok GUC Intel i915 sudah ada di $config_file. Tidak ditambahkan lagi."
+fi
+;;
+*)
+;;
+esac
 sudo pacman -S chaotic-aur/hyprland
+sudo pacman -S ttf-jetbrains-mono-nerd kitty hyprlock hypridle wofi brightnessctl pamixer dunst libnotify jq fastfetch kitty mpv easyeffects ladspa pipewire lsp-plugins pulsemixer gst-plugin-pipewire pipewire-alsa pipewire-jack wireplumber pipewire-pulse swww waybar wlsunset udiskie blueman wlr-randr xdg-desktop-portal-hyprland xdg-desktop-portal-gtk 
+
