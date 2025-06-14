0fd83a44c949f87320f0ab397f685d3c4a30b814
erickmufath <90117993+erickmufath@users.noreply.github.com>
2025-06-14 10:32:16 +0700

    Update install-dependencies.sh

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -22,14 +22,13 @@ fi
 N|n|No|NO|no)
 ;;
 esac
+
 sudo pacman -S ttf-jetbrains-mono-nerd kitty hyprlock hypridle wofi brightnessctl pamixer dunst libnotify jq fastfetch kitty mpv easyeffects ladspa pipewire lsp-plugins pulsemixer gst-plugin-pipewire pipewire-alsa pipewire-jack wireplumber pipewire-pulse libvdpau-va-gl swww waybar wlsunset udiskie blueman wlr-randr xdg-desktop-portal-hyprland xdg-desktop-portal-gtk 
 echo    "--------------------------------------------------------"
 read -p    "->] Install Chaotic-AUR [Y/n] : " chaur
 echo    "--------------------------------------------------------"
 case $chaur in
 Y|y|Yes|YES|yes|*)
-sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
-sudo pacman-key --lsign-key 3056513887B78AEB
 # Teks yang ingin ditambahkan
 block_to_add=$(cat <<EOF
 [chaotic-aur]
@@ -42,8 +41,15 @@ config_file="/etc/pacman.conf"
 if ! grep -Fxq "[chaotic-aur]" "$config_file"; then
     echo "Menambahkan repository chaotic-aur ke $config_file..."
     echo "$block_to_add" | sudo tee -a "$config_file" > /dev/null
+    sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+    sudo pacman-key --lsign-key 3056513887B78AEB
 else
     echo "Blok chaotic-aur sudah ada di $config_file. Tidak ditambahkan lagi."
 fi
+;;
+N|n|No|NO|no)
+;;
+esac
+
 sudo pacman -Syu
 sudo pacman -S chaotic-aur/hyprland
