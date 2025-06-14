d771f21174ceed4c1f1e4ce753cfdc70d85eb1be
erickmufath <90117993+erickmufath@users.noreply.github.com>
2025-06-14 09:16:55 +0700

    Update install-dependencies.sh

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,3 +1,49 @@
-pacman -S ttf-jetbrains-mono-nerd kitty
-read -p "CTRL+C dan install dulu Chaotic-AUR jika belum..." go
-sudo pacman -S chaotic-aur/hyprland hyprlock hypridle wofi brightnessctl pamixer dunst libnotify jq fastfetch kitty mpv easyeffects ladspa pipewire gst-plugin-pipewire pipewire-alsa pipewire-jack wireplumber pipewire-pulse libvdpau-va-gl vulkan-intel intel-ucode swww waybar wlsunset udiskie blueman wlr-randr intel-media-driver intel-media-sdk xdg-desktop-portal-hyprland xdg-desktop-portal-gtk 
+echo    "--------------------------------------------------------"
+read -p    "->] Install Intel Package [Y/n] : " intelpkg
+echo    "--------------------------------------------------------"
+case $intelpkg in
+Y|y|Yes|YES|yes|*)
+vulkan-intel intel-ucode intel-media-driver intel-media-sdk
+# Teks yang ingin ditambahkan
+block_to_add=$(cat <<EOF
+options i915 enable_guc=3
+EOF
+)
+# File konfigurasi yang ditarget
+config_file="/etc/modprobe.d/i915.conf"
+# Cek apakah blok sudah ada
+if ! grep -Fxq "[chaotic-aur]" "$config_file"; then
+    echo "Menambahkan repository chaotic-aur ke $config_file..."
+    echo "$block_to_add" | sudo tee -a "$config_file" > /dev/null
+else
+    echo "Blok GUC Intel i915 sudah ada di $config_file. Tidak ditambahkan lagi."
+fi
+;;
+N|n|No|NO|no)
+;;
+esac
+sudo pacman -S ttf-jetbrains-mono-nerd kitty hyprlock hypridle wofi brightnessctl pamixer dunst libnotify jq fastfetch kitty mpv easyeffects ladspa pipewire lsp-plugins pulsemixer gst-plugin-pipewire pipewire-alsa pipewire-jack wireplumber pipewire-pulse libvdpau-va-gl swww waybar wlsunset udiskie blueman wlr-randr xdg-desktop-portal-hyprland xdg-desktop-portal-gtk 
+echo    "--------------------------------------------------------"
+read -p    "->] Install Chaotic-AUR [Y/n] : " chaur
+echo    "--------------------------------------------------------"
+case $chaur in
+Y|y|Yes|YES|yes|*)
+sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+sudo pacman-key --lsign-key 3056513887B78AEB
+# Teks yang ingin ditambahkan
+block_to_add=$(cat <<EOF
+[chaotic-aur]
+Include = /etc/pacman.d/chaotic-mirrorlist
+EOF
+)
+# File konfigurasi yang ditarget
+config_file="/etc/pacman.conf"
+# Cek apakah blok sudah ada
+if ! grep -Fxq "[chaotic-aur]" "$config_file"; then
+    echo "Menambahkan repository chaotic-aur ke $config_file..."
+    echo "$block_to_add" | sudo tee -a "$config_file" > /dev/null
+else
+    echo "Blok chaotic-aur sudah ada di $config_file. Tidak ditambahkan lagi."
+fi
+sudo pacman -Syu
+sudo pacman -S chaotic-aur/hyprland
