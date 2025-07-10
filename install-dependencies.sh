ad21887c8f35feec820082f3d23e515e7d6b1f8e
erickmufath <90117993+erickmufath@users.noreply.github.com>
2025-07-10 21:12:53 +0700

    Menambahkan Autorun Hyprland di bash

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -4,21 +4,21 @@ echo    "--------------------------------------------------------"
 case $chaur in
 *)
 # Teks yang ingin ditambahkan
-block_to_add=$(cat <<EOF
+block_to_add_chaotic=$(cat <<EOF
 [chaotic-aur]
 Include = /etc/pacman.d/chaotic-mirrorlist
 EOF
 )
 # File konfigurasi yang ditarget
-config_file="/etc/pacman.conf"
+config_file_chaotic="/etc/pacman.conf"
 # Cek apakah blok sudah ada
-if ! grep -Fxq "[chaotic-aur]" "$config_file"; then
-    echo "Menambahkan repository chaotic-aur ke $config_file..."
-    echo "$block_to_add" | sudo tee -a "$config_file" > /dev/null
+if ! grep -Fxq "[chaotic-aur]" "$config_file_chaotic"; then
+    echo "Menambahkan repository chaotic-aur ke $config_file_chaotic..."
+    echo "$block_to_add_chaotic" | sudo tee -a "$config_file_chaotic" > /dev/null
     sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
     sudo pacman-key --lsign-key 3056513887B78AEB
 else
-    echo "Blok chaotic-aur sudah ada di $config_file. Tidak ditambahkan lagi."
+    echo "Blok chaotic-aur sudah ada di $config_file_chaotic. Tidak ditambahkan lagi."
 fi
 ;;
 N|n|NO|No|no)
@@ -47,19 +47,19 @@ sudo pacman -S vulkan-intel intel-ucode intel-media-driver intel-media-sdk intel
 echo 'export LIBVA_DRIVER_NAME=iHD' >> ~/.bashrc
 source ~/.bashrc
 # Teks yang ingin ditambahkan
-block_to_add=$(cat <<EOF
+block_to_add_intel=$(cat <<EOF
 options i915 enable_guc=3
 EOF
 )
 # File konfigurasi yang ditarget
-config_file="/etc/modprobe.d/i915.conf"
+config_file_intel="/etc/modprobe.d/i915.conf"
 # Cek apakah blok sudah ada
-if ! grep -Fxq "options i915 enable_guc=3" "$config_file"; then
-    echo "Menambahkan GUC intel i915 ke $config_file agar aktif..."
-    echo "$block_to_add" | sudo tee -a "$config_file" > /dev/null
+if ! grep -Fxq "options i915 enable_guc=3" "$config_file_intel"; then
+    echo "Menambahkan GUC intel i915 ke $config_file_intel agar aktif..."
+    echo "$block_to_add_intel" | sudo tee -a "$config_file_intel" > /dev/null
     sudo mkinitcpio -p linux
 else
-    echo "Blok GUC Intel i915 sudah ada di $config_file. Tidak ditambahkan lagi."
+    echo "Blok GUC Intel i915 sudah ada di $config_file_intel. Tidak ditambahkan lagi."
 fi
 ;;
 N|n|NO|No|no)
@@ -69,3 +69,25 @@ esac
 sudo pacman -S chaotic-aur/hyprland --needed
 sudo pacman -S ttf-jetbrains-mono-nerd kitty hyprutils hyprlock hypridle wofi brightnessctl pamixer dunst libnotify jq fastfetch kitty mpv easyeffects ladspa pipewire lsp-plugins pulsemixer gst-plugin-pipewire pipewire-alsa pipewire-jack wireplumber pipewire-pulse swww waybar wlsunset udiskie blueman wlr-randr xdg-desktop-portal-hyprland xdg-desktop-portal-gtk sof-firmware --needed
 sudo systemctl enable --global pipewire pipewire-pulse
+
+# Teks yang ingin ditambahkan
+block_to_add_hypr=$(cat <<EOF
+if [[ -z $DISPLAY && $(tty) = /dev/tty1 ]]; then
+    exec Hyprland
+fi
+EOF
+)
+# File konfigurasi yang ditarget
+config_file_hypr="~/.bash_profile"
+# Cek apakah blok sudah ada
+if ! grep -Fxq "exec Hyprland" "$config_file_hypr"; then
+    echo "Menambahkan Autorun Hyprland ke $config_file_hypr"
+    echo "$block_to_add_hypr" | sudo tee -a "$config_file_hypr" > /dev/null
+    sudo mkinitcpio -p linux
+else
+    echo "Autorun sudah ada di $config_file_hypr. Tidak ditambahkan lagi."
+fi
+;;
+N|n|NO|No|no)
+;;
+esac
